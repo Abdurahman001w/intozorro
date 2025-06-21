33f39f8569a2010bf66360ac0ba8bf0239817843
blacknesss <hikky93@mail.ru>
2025-06-21 19:42:42 +0300

    architecture

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+// import { classNames } from 'shared/lib/classNames/classNames';
+// import cls from './index.module.scss';
+
+// interface indexProps {
+//     className?: string;
+// }
+
+// export const index = ({className}: indexProps) => {
+//     return (
+//         <div className={classNames(cls.index, {}, [className!])}>
+            
+//         </div>
+//     );
+// };
