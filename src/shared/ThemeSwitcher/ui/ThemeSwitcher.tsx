2fb5995f71c31206f6f7c1130e43baa5cbee0707
abdurahman <atger@gmail.com>
2025-06-25 15:21:38 +0300

    add-button-15-20

--- src/shared/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 import cls from "./ThemeSwitcher.module.scss";
-import { Theme, useTheme } from "app/providers/ThemeProvider";
+//import { Theme, useTheme } from "app/providers/ThemeProvider";\
 import LightIcon from "shared/assets/icons/LightIcons.svg";
 import DarkIcon from "shared/assets/icons/DarkIons.svg";
 import {
@@ -12,18 +12,20 @@ interface ThemeSwitcherProps {
   className?: string;
 }
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
-  const { theme, toggleTheme } = useTheme();
+  // const { theme, toggleTheme } = useTheme();
   return (
     <Button
       theme={ThemeButton.CLEAR}
       className={classNames(cls.ThemeSwitcher, {}, [className])}
-      onClick={toggleTheme}
+      //   onClick={toggleTheme}
     >
-      {theme === Theme.DARK ? (
-        <DarkIcon width={20} />
-      ) : (
-        <LightIcon width={20} />
-      )}
+      {
+        //theme === Theme.DARK ? (
+        // <DarkIcon width={20} />
+        //) : (
+        // <LightIcon width={20} />
+        // )
+      }
     </Button>
   );
 };
